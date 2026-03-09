1e2922f9ef55483599b16ab64ef4fe1c26a2f46c
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2026-03-09 11:39:23 +0800

    feat(main.py): initialize task list and task class

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 #import FastAPI
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 #Initiate it
 app =FastAPI()
+
+#Initialize empty task list
+tasks = []
 
 #Define the root endpoint
 @app.get("/")
 def read_root():
     return{"Hello": "World"}
+
+#Define the task model
+class Task(BaseModel):
+    id: Optional[int] = None
+    description: str
